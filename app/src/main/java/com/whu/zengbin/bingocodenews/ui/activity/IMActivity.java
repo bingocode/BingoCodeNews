c08bc328930c0fe3e962b574be16a1fd2745249f
zengbin <2416940975@qq.com>
2018-08-02 16:52:55 +0800

    add IMActivity

---
+++ app/src/main/java/com/whu/zengbin/bingocodenews/ui/activity/IMActivity.java
@@ -0,0 +1,15 @@
+package com.whu.zengbin.bingocodenews.ui.activity;
+
+import android.os.Bundle;
+import com.whu.zengbin.bingocodenews.BaseActivity;
+import com.whu.zengbin.bingocodenews.R;
+
+public class IMActivity extends BaseActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_im);
+
+  }
+}
